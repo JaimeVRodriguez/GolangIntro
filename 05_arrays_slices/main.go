a6a5c4af81e8fefae6669321e91619def2f39d10
JaimeVRodriguez <57820323+JaimeVRodriguez@users.noreply.github.com>
2020-09-10 19:52:44 -0400

    Finish lesson 5

--- 05_arrays_slices/main.go
+++ 05_arrays_slices/main.go
@@ -11,6 +11,17 @@ func main() {
 	fruitArray[0] = "Apple"
 	fruitArray[1] = "Orange"
 
+	// Declare and assign
+	drinkArray := [2]string{"Water", "Mt. Dew"}
+
+	// Slice, similar to array but does not need a predfined length
+	candySlice := []string{"Red Vines", "Gushers", "Gummy Bears"}
+
 	fmt.Println(fruitArray)
 	fmt.Println(fruitArray[1])
+
+	fmt.Println(drinkArray)
+
+	fmt.Println(candySlice)
+	fmt.Println(len(candySlice))
 }
